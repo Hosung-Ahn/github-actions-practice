c2035c9f53920cb89c175969175f92ee15ea6ba0
HOSUNG <an3735297@pusan.ac.kr>
2024-08-27 17:21:12 +0900

    Create Spring Project

---
+++ gap/src/main/java/com/example/gap/GapApplication.java
@@ -0,0 +1,13 @@
+package com.example.gap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GapApplication.class, args);
+	}
+
+}
---
+++ gap/src/test/java/com/example/gap/GapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
